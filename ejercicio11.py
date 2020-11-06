505a456573130f9b714f5cd2ad33e374e0dcd161
Abel Antonio <abelantonios8520@gmail.com>
2020-11-05 23:52:54 -0500

    ejercicios

---
+++ ejercicio11.py
@@ -0,0 +1,8 @@
+num= int(input(" Ingrese numero "))
+div= int(input(" Ingrese segundo numero "))
+
+if (div!=0):
+    dividir=num/div
+    print("El resultado es: ",dividir)
+else:
+    print("El dividendo debe ser diferente a cero")    
---
+++ ejercico4.py
@@ -0,0 +1,6 @@
+var = int(input("Ingrese un número "))
+var2 = int(input("Ingrese segundo número "))
+
+product=var*var2
+
+print("El producto es: ", product)
